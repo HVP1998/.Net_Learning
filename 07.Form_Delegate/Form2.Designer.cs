ce6429530f789036510bba7ef37921d17e42b8b4
HVP1998 <83594060+HVP1998@users.noreply.github.com>
2021-11-06 21:40:27 +0800

    Add files via upload

---
+++ 07.Form_Delegate/Form2.Designer.cs
@@ -0,0 +1,73 @@
+﻿
+namespace _07.Form_Delegate
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ButOpenForm2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // ButOpenForm2
+            // 
+            this.ButOpenForm2.Location = new System.Drawing.Point(450, 194);
+            this.ButOpenForm2.Name = "ButOpenForm2";
+            this.ButOpenForm2.Size = new System.Drawing.Size(75, 23);
+            this.ButOpenForm2.TabIndex = 1;
+            this.ButOpenForm2.Text = "Send Value";
+            this.ButOpenForm2.UseVisualStyleBackColor = true;
+            this.ButOpenForm2.Click += new System.EventHandler(this.ButOpenForm2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(168, 194);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.ButOpenForm2);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form2_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ButOpenForm2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
