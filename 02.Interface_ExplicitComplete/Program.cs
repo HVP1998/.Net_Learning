88a749c224e793cb1f1761e1be6d13d3fa4c0e22
HVP1998 <83594060+HVP1998@users.noreply.github.com>
2021-11-06 21:38:26 +0800

    Add files via upload

---
+++ 02.Interface_ExplicitComplete/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Interface_ExplicitComplete
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bird b = new Bird();
+            b.Fly();
+            IFlyable i = new Bird();
+            i.Fly();
+            Console.ReadKey();
+        }
+    }
+    interface IFlyable
+    {
+        void Fly();
+    }
+    public class Bird:IFlyable
+    {
+        public void Fly()
+        {
+            Console.WriteLine("Bird Fly");
+        }
+        void IFlyable.Fly()
+        {
+            Console.WriteLine("Interface Complete");
+        }
+    }
+}
