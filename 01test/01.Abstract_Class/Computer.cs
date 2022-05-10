02348552c064cdd7428f1a2a4df22b6e09623edb
HVP1998 <83594060+HVP1998@users.noreply.github.com>
2022-05-10 10:26:01 +0800

    Add files via upload

---
+++ 01test/01.Abstract_Class/Computer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Abstract_Class
+{
+    public class Computer
+    {
+        public MobileStorage Ms { set ; get; }
+        public void Read()
+        {
+            this.Ms.Read();
+        }
+        public void Write()
+        {
+            this.Ms.Write();
+        }
+    }
+}
---
+++ 01test/04.File_Stream/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.File_Stream
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string source = @"1.mp4";
+            string target = @"2.mp4";
+            FileCopy(source, target);
+            Console.WriteLine("Copy Complete");
+            Console.ReadKey();
+        }
+        public static void FileCopy(string source, string target)
+        {
+            using(FileStream fsread=new FileStream(source,FileMode.Open,FileAccess.Read))
+            {
+                using(FileStream fswrite=new FileStream(target,FileMode.Create,FileAccess.Write))
+                {
+                    byte[] buffer = new byte[5 * 1024 * 1024];
+                    int r;
+                    while(true)
+                    {
+                        r = fsread.Read(buffer, 0, buffer.Length);
+                        if(r==0)
+                        {
+                            break;
+                        }
+                        fswrite.Write(buffer, 0, r);
+                    }
+                }
+            }
+        }
+    }
+}
