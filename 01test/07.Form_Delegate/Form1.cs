3ee712c31363053c75294574aec5d37637c7992d
HVP1998 <83594060+HVP1998@users.noreply.github.com>
2022-05-10 10:27:53 +0800

    Add files via upload

---
+++ 01test/07.Form_Delegate/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _07.Form_Delegate
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ButOpenForm2_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = Form2.GetSingleForm2(ShowText);
+            form2.Show();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LabText.Text = "***************";
+        }
+        public void ShowText(string str)
+        {
+            LabText.Text = str;
+        }
+    }
+}
---
+++ 01test/08.String_/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08.String_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str="12sdfs-5444,sdfs";
+            char[] c = { '-', ',' };
+            string[] str_array = str.Split(c);
+            foreach (var item in str_array)
+            {
+                Console.WriteLine(item);
+            }
+            string str1 = " 1 22 5555555   ";
+            string str1_trimstart= str1.TrimStart();
+            Console.WriteLine(str1_trimstart);
+            string str1_trimend = str1.TrimEnd();
+            Console.Write(str1_trimend);
+            string str1_trim = str1.Trim();
+            Console.Write(str1_trim);
+            Console.WriteLine(str.Length);
+            Console.WriteLine(str.ToUpper());
+            Console.WriteLine(str.ToLower());
+            Console.WriteLine(str.Contains("s"));
+            Console.WriteLine(str.StartsWith("s"));
+            Console.WriteLine(str.EndsWith("s"));
+            Console.WriteLine(str.Substring(0, 3));
+            Console.WriteLine(string.Join("", str_array));
+            Console.WriteLine(str.IndexOf('s'));
+            Console.WriteLine(str.LastIndexOf('s'));
+            string[] str2 = new string[3];
+            foreach (var item in str2)
+            {
+                Console.WriteLine(item);
+            }
+            double[] nums = new double[3];
+            foreach (var item in nums)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
