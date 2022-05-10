e58c180e7a2bf1b0e70e8963db5d1b6180195e2f
HVP1998 <83594060+HVP1998@users.noreply.github.com>
2022-05-10 11:06:02 +0800

    Add files via upload

---
+++ 03test/09.List_/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+
+namespace _09.List_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable ht = new Hashtable();
+            ht[0] = 5;
+            ht.Add(1,6);
+            foreach (var item in ht)
+            {
+                Console.WriteLine(item.ToString());
+            }
+            Console.ReadKey();
+        }
+    }
+}
